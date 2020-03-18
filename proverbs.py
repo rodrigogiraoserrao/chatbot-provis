7264d1a9ca7c10492993ebdac1ed31c26a42d3b5
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-03-18 22:54:35 +0000

    Add one proverb.

--- proverbs.py
+++ proverbs.py
@@ -3,7 +3,9 @@ proverbs = [
     (
         "\U0001f914 \U000026b0 \U0001f434", # thinking face + coffin + horse face
         "proverb_thinking_killed_the_donkey"
+    ),
+    (
+        "\U0001F40E \U0001F3B2 \U0001F648 \U0001F600", # horse + die + see-no-evil monkey + grinning face
+        "proverb_dont_check_teeth_of_gifted_horse"
     )
 ]
-
-proverbs = []
