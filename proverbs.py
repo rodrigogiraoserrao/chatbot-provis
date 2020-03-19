58afe7dfef68ab65755d077e6678f5dc40d7e997
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-03-19 00:10:37 +0000

    Change proverb  structure

--- proverbs.py
+++ proverbs.py
@@ -1,11 +1,13 @@
 # Unicode code points taken from http://www.unicode.org/emoji/charts/full-emoji-list.html
 proverbs = [
-    (
-        "\U0001f914 \U000026b0 \U0001f434", # thinking face + coffin + horse face
-        "proverb_thinking_killed_the_donkey"
-    ),
-    (
-        "\U0001F40E \U0001F3B2 \U0001F648 \U0001F600", # horse + die + see-no-evil monkey + grinning face
-        "proverb_dont_check_teeth_of_gifted_horse"
-    )
+    {
+        "id": 1,
+        "emojies": "\U0001f914 \U000026b0 \U0001f434", # thinking face + coffin + horse face
+        "intent": "proverb_thinking_killed_the_donkey"
+    },
+    {
+        "id": 2,
+        "emojies": "\U0001F40E \U0001F3B2 \U0001F648 \U0001F600", # horse + die + see-no-evil monkey + grinning face
+        "intent": "proverb_dont_check_teeth_of_gifted_horse"
+    }
 ]
