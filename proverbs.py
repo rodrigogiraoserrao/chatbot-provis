bd52d0c3b15d1ddca64604d5e2f5bd80bbd73d43
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-03-20 19:27:27 +0000

    Add 5 proverbs.

--- proverbs.py
+++ proverbs.py
@@ -2,17 +2,52 @@
 proverbs = [
     {
         "id": 1,
-        "emojies": "\U0001f914 \U000026b0 \U0001f434", # thinking face + coffin + horse face
-        "intent": "proverb_thinking_killed_the_donkey"
-    },
-    {
-        "id": 2,
-        "emojies": "\U0001F40E \U0001F3B2 \U0001F648 \U0001F600", # horse + die + see-no-evil monkey + grinning face
+        "emojies": "\U0001f40e \U0001f3b2 \U0001f648 \U0001f601", # (HORSE) (GAME DIE) (SEE-NO-EVIL MONKEY) (GRINNING FACE WITH SMILING EYES)
         "intent": "proverb_dont_check_teeth_of_gifted_horse"
     },
     {
-        "id": 3,
-        "emojies": "\U0001F30D \U0001F648 \U0001F61C \U0001F451", # globe showing Europe-Africa + see-no-evil monkey + winking face with tongue + crown
+        "id": 2,
+        "emojies": "\U0001F30D \U0001F648 \U0001F61C \U0001F451", # (EARTH GLOBE EUROPE-AFRICA) (SEE-NO-EVIL MONKEY) (FACE WITH STUCK-OUT TONGUE AND WINKING EYE) (CROWN)
         "intent": "proverb_one_eye_rules_over_the_blind"
+    },
+    {
+        "id": 3,
+        "emojies": "\U0001f622 \U0001f44e \U0001f37c", # (CRYING FACE) (THUMBS DOWN SIGN) (BABY BOTTLE)
+        "intent": "proverb_no_crying_no_breastfeeding"
+    },
+    {
+        "id": 4,
+        "emojies": "\U0001f64a \u26d4 \U0001f41d", # (SPEAK-NO-EVIL MONKEY) (NO ENTRY) (HONEYBEE)
+        "intent": "proverb_mouth_shut_enters_no_fly"
+    },
+    {
+        "id": 5,
+        "emojies": "\u2795 \U0001f425 \u270b \U0001f425 \U0001f425 \u2708", # (HEAVY PLUS SIGN) (FRONT-FACING BABY CHICK) (RAISED HAND) (FRONT-FACING BABY CHICK) (FRONT-FACING BABY CHICK) (AIRPLANE)
+        "intent": "proverb_better_one_here_than_two_there"
+    },
+    {
+        "id": 6,
+        "emojies": "\U0001f607 \U0001f3e0 \U0001f44e \U0001f64f", # ['(SMILING FACE WITH HALO)', '(HOUSE BUILDING)', '(THUMBS DOWN SIGN)', '(PERSON WITH FOLDED HANDS)']
+        "intent": "proverb_house_saints_dont_make_miracles"
+    },
+    {
+        "id": 7,
+        "emojies": "\u23f0 \U0001f60b \U0001f363 \U0001f525 \U0001f444", # ['(ALARM CLOCK)', '(FACE SAVOURING DELICIOUS FOOD)', '(SUSHI)', '(FIRE)', '(MOUTH)']
+        "intent": "proverb_hurry_eat_raw_or_burn_mouth"
+    },
+    {
+        "id": 8,
+        "emojies": "\U0001f343 \U0001f4a8 \u2614 \u26a1", # ['(LEAF FLUTTERING IN WIND)', '(DASH SYMBOL)', '(UMBRELLA WITH RAIN DROPS)', '(HIGH VOLTAGE SIGN)']
+        "intent": "proverb_sow_winds_harvest_tempests"
+    },
+    {
+        "id": 9,
+        "emojies": "\U0001f412 \U0001f334 \U0001f412 \U0001f334", # ['(MONKEY)', '(PALM TREE)', '(MONKEY)', '(PALM TREE)']
+        "intent": "proverb_each_monkey_to_its_tree"
+    },
+    {
+        "id": 10,
+        "emojies": "\u23f3 \U0001f6b6 \U0001f47a \U0001f46c", # ['(HOURGLASS WITH FLOWING SAND)', '(PEDESTRIAN)', '(JAPANESE GOBLIN)', '(TWO MEN HOLDING HANDS)']
+        "intent": "proverb_better_alone_than_in_bad_company"
     }
 ]
