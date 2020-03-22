570225011b9e98f055a6580ccdd6e3f9b53d6bc4
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-03-22 20:06:50 +0000

    Change capitalization of proverb hint.

--- proverbs.py
+++ proverbs.py
@@ -3,51 +3,51 @@ proverbs = {
     1: {
         "emojis": "\U0001f40e \U0001f3b2 \U0001f648 \U0001f601", # (HORSE) (GAME DIE) (SEE-NO-EVIL MONKEY) (GRINNING FACE WITH SMILING EYES)
         "intent": "proverb_dont_check_teeth_of_gifted_horse",
-        "hint": "Este cavalo foi uma oferta..."
+        "hint": "este cavalo foi uma oferta..."
     },
     2: {
         "emojis": "\U0001F30D \U0001F648 \U0001F61C \U0001F451", # (EARTH GLOBE EUROPE-AFRICA) (SEE-NO-EVIL MONKEY) (FACE WITH STUCK-OUT TONGUE AND WINKING EYE) (CROWN)
         "intent": "proverb_one_eye_rules_over_the_blind",
-        "hint": "Eu tenho dois olhos!"
+        "hint": "eu tenho dois olhos!"
     },
     3: {
         "emojis": "\U0001f622 \U0001f44e \U0001f37c", # (CRYING FACE) (THUMBS DOWN SIGN) (BABY BOTTLE)
         "intent": "proverb_no_crying_no_breastfeeding",
-        "hint": "Não choras..?"
+        "hint": "não choras..?"
     },
     4: {
         "emojis": "\U0001f64a \u26d4 \U0001f41d", # (SPEAK-NO-EVIL MONKEY) (NO ENTRY) (HONEYBEE)
         "intent": "proverb_mouth_shut_enters_no_fly",
-        "hint": "Fecha a boca!"
+        "hint": "fecha a boca!"
     },
     5: {
         "emojis": "\u2795 \U0001f425 \u270b \U0001f425 \U0001f425 \u2708", # (HEAVY PLUS SIGN) (FRONT-FACING BABY CHICK) (RAISED HAND) (FRONT-FACING BABY CHICK) (FRONT-FACING BABY CHICK) (AIRPLANE)
         "intent": "proverb_better_one_here_than_two_there",
-        "hint": "Mais vale...?"
+        "hint": "mais vale...?"
     },
     6: {
         "emojis": "\U0001f607 \U0001f3e0 \U0001f44e \U0001f64f", # ['(SMILING FACE WITH HALO)', '(HOUSE BUILDING)', '(THUMBS DOWN SIGN)', '(PERSON WITH FOLDED HANDS)']
         "intent": "proverb_house_saints_dont_make_miracles",
-        "hint": "Acreditas em milagres?"
+        "hint": "acreditas em milagres?"
     },
     7: {
         "emojis": "\u23f0 \U0001f60b \U0001f363 \U0001f525 \U0001f444", # ['(ALARM CLOCK)', '(FACE SAVOURING DELICIOUS FOOD)', '(SUSHI)', '(FIRE)', '(MOUTH)']
         "intent": "proverb_hurry_eat_raw_or_burn_mouth",
-        "hint": "Eu gosto de viver calmamente..."
+        "hint": "eu gosto de viver calmamente..."
     },
     8: {
         "emojis": "\U0001f343 \U0001f4a8 \u2614 \u26a1", # ['(LEAF FLUTTERING IN WIND)', '(DASH SYMBOL)', '(UMBRELLA WITH RAIN DROPS)', '(HIGH VOLTAGE SIGN)']
         "intent": "proverb_sow_winds_harvest_tempests",
-        "hint": "A prática da agricultura é incrível!"
+        "hint": "a prática da agricultura é incrível!"
     },
     9: {
         "emojis": "\U0001f412 \U0001f334 \U0001f412 \U0001f334", # ['(MONKEY)', '(PALM TREE)', '(MONKEY)', '(PALM TREE)']
         "intent": "proverb_each_monkey_to_its_tree",
-        "hint": "Sabes o que são galhos?"
+        "hint": "sabes o que são galhos?"
     },
     10: {
         "emojis": "\u23f3 \U0001f6b6 \U0001f47a \U0001f46c", # ['(HOURGLASS WITH FLOWING SAND)', '(PEDESTRIAN)', '(JAPANESE GOBLIN)', '(TWO MEN HOLDING HANDS)']
         "intent": "proverb_better_alone_than_in_bad_company",
-        "hint": "Eu cá não gosto muito de andar sozinho..."
+        "hint": "eu cá não gosto muito de andar sozinho..."
     }
 }
